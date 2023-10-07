b9dc8e245350248a49093679491d72f513c3457a
kmmaniez <dzlabsid@gmail.com>
2023-10-07 17:52:56 +0700

    add components statistik

---
+++ resources/views/components/public/statistik.blade.php
@@ -0,0 +1,30 @@
+<div class="tab-content mt-3" id="myTabContent">
+    <div class="tab-pane" id="statistic-tab-pane" role="tabpanel" aria-labelledby="statistic-tab" tabindex="0">
+        <div class="card border-0">
+            <div class="card-body p-2">
+                {{-- <h4>Statistik Produk</h4> --}}
+
+                <div class="statistik-produk d-flex flex-column gap-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5>Total Penjualan</h5>
+                            <h6 class="text-success fw-bold">Rp. 2.150.000</h6>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5>Total Pendapatan</h5>
+                            <h6 class="text-success fw-bold">Rp. 2.150.000</h6>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5>Total Produk Terjual</h5>
+                            <h6 class="text-success fw-bold">125 Foto</h6>                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
