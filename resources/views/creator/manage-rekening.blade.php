1ea8cb3f37c35f3d27536ab2c296f633f1b2bb0d
kmmaniez <dzlabsid@gmail.com>
2023-10-08 22:00:18 +0700

    change views managerek

--- resources/views/creator/manage-rekening.blade.php
+++ resources/views/creator/manage-rekening.blade.php
@@ -209,14 +209,19 @@
                 <div class="vstack mt-3">
                     <div class="card">
                         <div class="card-header bg-transparent">
-                            <span class="fs-5 text-secondary">Indonesian Bank Transfer</span>
+                            <h5 class="text-secondary">Indonesian Bank Transfer</h5>
                         </div>
                         <div class="card-body pb-4">
                             <form action="" method="post">
                                 @csrf
                                 <div class="form-group">
                                     <label for="">Nama Bank</label>
-                                    <input class="form-control shadow-none" placeholder="BCA/BNI..." type="text" name="" id="">
+                                    <select class="form-control shadow-none" name="" id="">
+                                        <option value="">BCA</option>
+                                        <option value="">BRI</option>
+                                        <option value="">BNI</option>
+                                        <option value="">MANDIRI</option>
+                                    </select>
                                 </div>
                                 <div class="form-group mt-3">
                                     <label for="">Nomor Rekening</label>
@@ -230,8 +235,8 @@
                         </div>
                     </div>
                     <div class="vstack mt-3 gap-2">
-                        <button class="btn btn-md bg-success bg-gradient text-white">Save Change</button>
-                        <button class="btn btn-md btn-outline-success">Cancel</button>
+                        <button class="btn btn-md bg-danger fw-semibold text-uppercase bg-gradient text-white">Save Change</button>
+                        <a href="{{ route('admin') }}" class="btn btn-md btn-outline-danger">Cancel</a>
                     </div>
                 </div>
     
