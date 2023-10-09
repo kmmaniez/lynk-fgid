53b6469657fc4ecb91440ff04e138e6bb1f3e174
kmmaniez <dzlabsid@gmail.com>
2023-10-09 19:56:26 +0700

    change order views

--- resources/views/creator/order.blade.php
+++ resources/views/creator/order.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.master')
 
 @section('content')
-           <div class="card border-0 mt-3">
-                <div class="card-body p-2">
-
-                    <div class="vstack w-100 gap-2">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5>Total Penjualan</h5>
-                                <h6 class="text-success fw-bold">Rp. 2.150.000</h6>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <h5>Total Pendapatan</h5>
-                                <h6 class="text-success fw-bold">Rp. 2.150.000</h6>
+    <section id="order" class="mt-3">
+        <div class="card border-0 mt-3">
+            <div class="card-body p-2">
+                <h5>History Order</h5>
+                <div class="vstack w-100 gap-2">
+                    @for ($i=0; $i < 10; $i++)
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="detail">
+                                <span class="fw-semibold">Tanggal : 24/10/2023</span>
+                                <div class="list-item my-2">
+                                    <span class="d-block">Detail Item :</span>
+                                    <ul class="m-0">
+                                        <li>Joki Valorant (1x) | Rp. 20.000</li>
+                                        @if ($i%3===0)
+                                        <li>Jasa Social Media (4x) | Rp. 5.000</li>
+                                        @endif
+                                    </ul>
+                                </div>
+                                <span class="d-block"><strong>Total : Rp. 25.000</strong></span>
+                                <span><strong>Email pelanggan : {{ Str::random(5) }}@gmail.com</strong></span>
+                                {{-- <span><i data-feather="copy" class="fa-24 text-primary"></i></span> --}}
                             </div>
                         </div>
                     </div>
+                    @endfor
                 </div>
             </div>
+        </div>
+    </section>
 @endsection
