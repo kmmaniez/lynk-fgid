33359386a883b59ccc63c24084bf6e6f4a79665b
kmmaniez <dzlabsid@gmail.com>
2023-10-18 16:08:26 +0700

    add migration transaction

---
+++ database/migrations/2023_10_17_032016_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->references('id')->on('products')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->integer('total_item');
+            $table->string('customer_email');
+            $table->string('payment_options');
+            $table->string('payment_status');
+            $table->date('date_transaction');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
