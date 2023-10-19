28e0d4c46b6c6d710b4539e85d891c51496bf744
kmmaniez <dzlabsid@gmail.com>
2023-10-19 12:48:29 +0700

    add link requests

---
+++ app/Http/Requests/Product/LinkProductRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use App\Enums\LayoutEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class LinkProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required','string'],
+            'thumbnail' => ['nullable','image','mimes:png,jpg,jpeg','max:2048'],
+            'url' => ['required','url'],
+            'layout' => ['required', new Enum(LayoutEnum::class)],
+        ];
+    }
+}
