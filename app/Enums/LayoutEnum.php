e6c743e393b8f473f69e9136657e565ac9859557
kmmaniez <dzlabsid@gmail.com>
2023-10-19 12:45:00 +0700

    add layout enum

---
+++ app/Enums/LayoutEnum.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Enums;
+
+enum LayoutEnum: string {
+    
+    case LAYOUT_DEFAULT = 'default'; // default
+    case LAYOUT_GRID = 'grid'; // grid
+    case LAYOUT_LARGE = 'large'; // large
+}
