72e9b7dca58d23c88e9ff2bdd0bc9d0c8858a75f
kmmaniez <dzlabsid@gmail.com>
2023-10-19 12:47:08 +0700

    add cta enum

---
+++ app/Enums/CtaEnum.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Enums;
+
+enum CtaEnum: int {
+
+    case CTA_NO_OPTION = 0; // I Want this 
+    case CTA_OPTION_1 = 1; // Support Creator 
+    case CTA_OPTION_2 = 2; // Beli Sekarang
+    case CTA_OPTION_3 = 3; // Book Now
+}
