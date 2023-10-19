0dbbbd8602598e5521a6553d1968ca4ffa8756cb
kmmaniez <dzlabsid@gmail.com>
2023-10-19 12:44:22 +0700

    add ulid to model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUlids;
     protected $with = ['users'];
 
     protected $fillable = [
@@ -24,6 +25,11 @@ class Product extends Model
         'CTA',
         'layout',
     ];
+
+    // protected $casts = [
+    //     'min_price' => 'integer',
+    //     'min_price' => 'integer',
+    // ];
 
     public function users()
     {
