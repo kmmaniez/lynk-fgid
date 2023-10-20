587fb4cb0f1610fd2e64a2b8fcb4cd5ce3b94346
kmmaniez <dzlabsid@gmail.com>
2023-10-20 16:42:46 +0700

    update views appearance

--- resources/views/creator/appearance.blade.php
+++ resources/views/creator/appearance.blade.php
@@ -49,6 +49,11 @@
             border-radius: 8px;
             cursor: pointer;
         }
+        #coverdisplay{
+            width: 800px;
+            height: 200px;
+            /* style="width: 100%; height: 200px;"  */
+        }
     </style>
 @endpush
 @section('content')
@@ -58,32 +63,33 @@
                 <span class="fs-5">Appearance Details</span>
             </div>
             <div class="card-body p-2">
-                <form action="" method="post">
+                <form action="{{ route('profile.update') }}" method="post" enctype="multipart/form-data">
                     @csrf
-                    <div class="form-group vstack align-items-center">
-                        <img src="{{ asset('assets/user1.jpg') }}" style="width: 100%; height: 200px;" class="object-fit-cover card-img-top" alt="Cover profile">
-                        <input class="form-control shadow-none mt-2" type="file" name="x" id="cover-image" hidden>
-                        <label id="custom" for="cover-image">Upload Cover Profile <i data-feather="image"></i></label>
+                    @method('PATCH')
+                    <div class="form-group p-2 vstack align-items-center" style="overflow: hidden">
+                        <img id="coverdisplay" src="{{ ($user->coverimage) ? Storage::url($user->coverimage) : asset('assets/user1.jpg') }}" class="object-fit-cover card-img-top" alt="Cover profile">
+                        <input class="form-control shadow-none mt-2" type="file" name="coverimage" id="coverimage" accept="image/*" hidden>
+                        <label id="custom" for="coverimage">{{ ($user->coverimage) ? 'Change Cover' : 'Upload Cover' }} <i data-feather="image"></i></label>
                     </div>
                     <div class="form-group vstack align-items-center">
-                        <img src="{{ asset('assets/user1.jpg') }}" class="profile-img card-img-top rounded-circle border border-secondary-tertiary border-5" alt="Foto profile">
-                        <input class="form-control shadow-none" type="file" name="x" id="profile-image" hidden>
-                        <label id="custom" for="profile-image">Upload Foto Profile <i data-feather="image"></i></label>
+                        <img id="photodisplay" src="{{ ($user->photo) ? Storage::url($user->photo) : asset('assets/user2.jpg') }}" class="profile-img card-img-top rounded-circle border border-secondary-tertiary border-5" alt="Foto profile">
+                        <input class="form-control shadow-none" type="file" name="photo" id="photo" accept="image/*" hidden>
+                        <label id="custom" for="photo">{{ ($user->photo) ? 'Change Photo' : 'Upload Photo' }} <i data-feather="image"></i></label>
                     </div>
                     <div class="form-group">
-                        <label for="x">Deskripsi profile</label>
-                        <textarea class="form-control shadow-none" name="x" id="x" cols="30" rows="2"></textarea>
+                        <label for="description">Profile description</label>
+                        <textarea class="form-control shadow-none" name="description" id="description" cols="30" rows="2">{{ $user->description }}</textarea>
                     </div>
                     <div class="form-group">
                         <label for="theme_profile">Theme profile (*Default: Light)</label>
                         <div class="hstack gap-3 mt-2" id="theme_profile">
                             <label>
-                                <input class="form-check-input" type="radio" name="theme" value="light" checked>
+                                <input class="form-check-input" type="radio" name="theme" value="light" {{ ($user->theme == "light") ? 'checked' : '' }}>
                                 <img src="{{ asset('assets/light-theme.png') }}" width="164" height="164" alt="light">
                                 <span>Light</span>
                             </label>
                             <label>
-                                <input class="form-check-input" type="radio" name="theme" value="dark">
+                                <input class="form-check-input" type="radio" name="theme" value="dark" {{ ($user->theme == "dark") ? 'checked' : '' }}>
                                 <img src="{{ asset('assets/dark-theme.png') }}" width="164" height="164" alt="dark">
                                 <span>Dark</span>
                             </label>
@@ -91,11 +97,29 @@
                     </div>
 
                     <div class="vstack gap-2">
-                        <button class="btn btn-md bg-danger bg-gradient text-white fw-semibold text-uppercase w-100 mt-3">Simpan</button>
-                        <a href="{{ route('admin') }}" class="btn w-100 fw-medium">Kembali</a>
+                        <button class="btn btn-md bg-danger bg-gradient text-white fw-semibold text-uppercase w-100 mt-3">Save Appearance</button>
+                        <a href="{{ route('admin') }}" class="btn w-100 fw-medium">Back</a>
                     </div>
                 </form>
             </div>
         </div>
     </section>
 @endsection
+@push('scripts')
+    <script>
+        $('#coverimage').change(function() {
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                $('#coverdisplay').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(this.files[0]);
+        });
+        $('#photo').change(function() {
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                $('#photodisplay').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(this.files[0]);
+        });
+    </script>
+@endpush
