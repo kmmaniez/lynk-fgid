3b347b35dbe2501de56c6fa05fe7d14ddcf76ba9
kmmaniez <dzlabsid@gmail.com>
2023-10-21 15:57:50 +0700

    update model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\ProductTypeEnum;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -14,6 +15,7 @@ class Product extends Model
     protected $fillable = [
         'user_id',
         'name',
+        'type',
         'slug',
         'thumbnail',
         'images',
@@ -22,14 +24,15 @@ class Product extends Model
         'min_price',
         'max_price',
         'messages',
-        'CTA',
+        'cta_text',
         'layout',
     ];
 
-    // protected $casts = [
-    //     'min_price' => 'integer',
-    //     'min_price' => 'integer',
-    // ];
+    protected $casts = [
+        'type' => ProductTypeEnum::class,
+        'min_price' => 'integer',
+        'max_price' => 'integer',
+    ];
 
     public function users()
     {
