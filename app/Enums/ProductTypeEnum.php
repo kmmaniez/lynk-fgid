3f33a3d96f41ec33c38fa369e0d0d598c0659c42
kmmaniez <dzlabsid@gmail.com>
2023-10-21 15:56:16 +0700

    update product type enum

--- app/Enums/ProductTypeEnum.php
+++ app/Enums/ProductTypeEnum.php
@@ -3,6 +3,6 @@ namespace App\Enums;
 
 enum ProductTypeEnum: string {
     
-    case PRODUCT_LINK = 'link'; // default
-    case PRODUCT_DIGITAL = 'digital'; // grid
+    case PRODUCT_LINK = 'link'; // link
+    case PRODUCT_DIGITAL = 'digital'; // digital
 }
