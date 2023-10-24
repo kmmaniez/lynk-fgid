d8bca07c205439afcb30aec6f1ecf8e05c99026f
kmmaniez <dzlabsid@gmail.com>
2023-10-25 05:13:25 +0700

    update layout enum

--- app/Enums/LayoutEnum.php
+++ app/Enums/LayoutEnum.php
@@ -6,4 +6,9 @@ enum LayoutEnum: string {
     case LAYOUT_DEFAULT = 'default'; // default
     case LAYOUT_GRID = 'grid'; // grid
     case LAYOUT_LARGE = 'large'; // large
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'name', 'value');
+    }
 }
