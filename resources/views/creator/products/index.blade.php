877ef4800cd193dd7f2936cfe271c0338118c836
kmmaniez <dzlabsid@gmail.com>
2023-10-30 13:01:15 +0700

    update page products user

--- resources/views/creator/products/index.blade.php
+++ resources/views/creator/products/index.blade.php
@@ -1,206 +1,5 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap5/css/bootstrap.min.css') }}">
-    <style>
-        body{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: #eaeaea;
-        }
-        .shopping{
-            padding: 4px 6px;
-            cursor: pointer;
-        }
-        #wrapper{
-            width: 800px;
-            position: relative;
-        }
-        footer{
-            width: 800px;
-            height: max-content;
-            margin: 0 auto;
-            border-top: 0.5px solid rgba(36, 35, 35, 0.1);
-        }
-    </style>
-    <script src="{{ asset('assets/feather-icons/dist/feather.js') }}"></script>
-
-</head>
-<body>
-    <div id="wrapper" class="container p-0 bg-white">
-        <nav class="navbar border-bottom navbar-expand-lg px-3 d-flex justify-content-between" style="height: 4rem;">
-            <a class="navbar-brand" href="{{ route('admin') }}">
-                <img src="{{ asset('assets/logo.png') }}" alt="Logo" width="150"
-                    class="d-inline-block align-text-top">
-            </a>
-            <div class="shopping">
-                <i data-feather="shopping-cart"></i> 
-            </div>
-        </nav>
-
-        <section id="wrap" class="pb-3 px-3">
-            <div class="header" style="">
-                <div class="card rounded-0 border-0 text-center">
-                    <div class="card-body vstack gap-3 align-items-center">
-                        <img src="{{ asset('assets/user6.jpg') }}" style="width: 5rem; height: 5rem;" 
-                            class="rounded-circle object-fit-cover" alt="...">
-                        <span><strong>@username</strong></span>
-                        <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, adipisci?</span>
-                    </div>
-                </div>
-            </div>
-
-            <div class="list-products mt-2 mb-5">
-                <div class="row gap-3">
-                    <div class="col-12">
-                        <a href="/awkarin/detail" class="text-decoration-none">
-                            <div class="card text-center">
-                                <div class="card-body">
-                                    <span>Lorem ipsum adipisicing elit. Repellat!</span>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-12">
-                        <a href="/awkarin/detail" class="text-decoration-none">
-                            <div class="card">
-                                <div class="card-body d-flex flex-start align-items-center gap-3">
-                                    <img src="{{ asset('assets/user1.jpg') }}" style="width: 4rem; height: 4rem;" class="card-img-top" alt="...">
-                                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat!</span>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-12">
-                        <div class="row">
-                            <div class="col">
-                                <a href="/awkarin/detail" class="text-decoration-none">
-                                    <div class="card h-100">
-                                        <img src="{{ asset('assets/user1.jpg') }}" style="width: 100%; height: 120px;"
-                                            class="card-img-top object-fit-cover" alt="...">
-                                        <div class="card-body">
-                                            <h6>Judul</h6>
-                                            <span><strong>Rp. 5.000</strong></span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="col">
-                                <a href="/awkarin/detail" class="text-decoration-none">
-                                    <div class="card h-100">
-                                        <img src="{{ asset('assets/user3.jpg') }}" style="width: 100%; height: 120px;"
-                                            class="card-img-top object-fit-cover" alt="...">
-                                        <div class="card-body">
-                                            <h6>Judul</h6>
-                                            <span><strong>Rp. 5.000</strong></span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <a href="/awkarin/detail" class="text-decoration-none">
-                            <div class="card h-100">
-                                <img src="{{ asset('assets/user2.jpg') }}" style="width: 100%; height: 240px;"
-                                    class="card-img-top object-fit-cover" alt="...">
-                                <div class="card-body">
-                                    <h6>Judul</h6>
-                                    <span><strong>Rp. 5.000</strong></span>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-12">
-                        <a href="/awkarin/detail" class="text-decoration-none">
-                            <div class="card text-center">
-                                <div class="card-body">
-                                    <span>Lorem ipsum adipisicing elit. Repellat!</span>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-12">
-                        <div class="row">
-                            <div class="col-6">
-                                <a href="/awkarin/detail" class="text-decoration-none">
-                                    <div class="card h-100">
-                                        <img src="{{ asset('assets/user6.jpg') }}" style="width: 100%; height: 120px;"
-                                            class="card-img-top object-fit-cover" alt="...">
-                                        <div class="card-body">
-                                            <h6>Judul</h6>
-                                            <span><strong>Rp. 5.000</strong></span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </section>
-
-    </div>
-
-    <script src="{{ asset('assets/vendor/bootstrap5/js/bootstrap.bundle.min.js') }}"></script>
-    <script>
-        feather.replace();
-    </script>
-</body>
-</html> --}}
 @extends('layouts.products')
 @push('styles')
-    {{-- <style>
-        /* body{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: #eaeaea;
-        } */
-        #wrapper{
-            overflow-y: scroll;
-        }
-        footer{
-            width: 800px;
-            height: max-content;
-            margin: 0 auto;
-            border-top: 0.5px solid rgba(36, 35, 35, 0.1);
-        }
-        .bg-dark-cover{
-            background-color: #121212;
-            color: #fff;
-        }
-        section#wrap{
-            padding: 0 1rem;/
-        }
-        @media (max-width: 820px) {
-            section#wrap{
-                padding: 0 0.5rem;
-            }
-        }
-        #coverprofile{
-            content: '';
-            background: url('{{ asset("assets/user6.jpg") }}');
-            object-fit: cover;
-            background-size: 100%;
-            background-position: center;
-            background-repeat: no-repeat;
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            z-index: -1;
-            opacity: 0.3;
-        }
-        .header span{
-            text-shadow: 0 0 10px rgb(38, 38, 38);
-        }
-    </style> --}}
     <style>
         .header span{
             user-select: none;
@@ -255,6 +54,32 @@
             left: 0;
             background-color: rgba(25, 25, 25, 0.615);
             z-index: -1;
+        }
+        .list-products{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            row-gap: 8px;
+        }
+        .item{
+            flex: 0 0 48%;
+            height: max-content;
+        }
+        .item.grid{
+            flex: 0 0 calc(100%);
+            /* background-color: bisque; */
+        }
+        @media screen and (max-width: 800px){
+            .list-products{
+                width: 100%;
+            }
+            .item.grid{
+                flex: 0 0 calc(100%);
+            }
+            .item{
+                flex: 0 0 48%; 
+                /* background-color: green; */
+            }
         }
     </style>
 @endpush
@@ -414,49 +239,85 @@
         </div>
 
         <div class="list-products mt-2">
-            <div class="row gap-2">
-                {{-- @dump($products) --}}
-                @foreach ($products as $product)
-                <div class="col-12">
+            <!-- NEW -->
+            @foreach ($products as $product)
+            <div class="item {{ ($product->layout->value === 'default') ? 'grid' : '' }}">
+                <a href="{{ ($product->type->value == 'link') ? $product->url : route('products.detailuser', [$user->username,$product->id]) }}" class="text-decoration-none">
+                    <div class="card">
+                        <div class="card-body {{ ($product->type->value == 'link') ? 'd-flex gap-2 align-items-center' : '' }}">
+                            @if ($product->type->value === 'link')
+                                @if ($product->thumbnail)
+                                <img src="{{ Storage::url($product->thumbnail) }}" style="width: 64px; height: 64px;" class="card-img-top rounded-2">
+                                @endif
+                                <h6 class="mt-3 {{ ($product->type->value == 'link') ? 'flex-grow-1 text-center' : '' }}">{{ $product->name }} {{ $product->type->value }}</h6>
+                            @else
+                                @if ($product->thumbnail)
+                                <img src="{{ Storage::url('tes/'.$product->thumbnail) }}" style="width: 100%; height: 120px;" class="rounded-1 object-fit-cover">
+                                @endif
+                                <h6 class="{{ ($product->thumbnail) ? 'mt-3' : '' }}">{{ $product->name }} {{ $product->type->value }}</h6>
+                                <span><strong>Rp. {{ $product->min_price }}</strong></span>
+                            @endif
+                            {{-- <small>{{ $product->id }}</small> --}}
+                        </div>
+                    </div>
+                </a>
+            </div>
+                
+            @endforeach
+            {{-- <div class="row lists gap-2"> --}}
+                {{-- @foreach ($products as $product)
+                <div 
+                class="{{ ($product->layout->value == "default" || $product->layout->value == "grid") ? 'col-12' : 'col-6' }}">
                     <a href="{{ ($product->slug) ? $product->slug : $product->url }}" title="{{ $product->name }}" class="text-decoration-none">
 
-                        @if ($product->layout === "large")
                         <div class="card {{ ($user->theme == "light") ? 'bg-white' : 'bg-dark text-white' }} h-100">
-                            <img src="{{ ($product->thumbnail) ? Storage::url($product->thumbnail) : asset('assets/user2.jpg') }}" style="width: 100%; height: 240px;"
-                                class="card-img-top" alt="...">
+
+                            @if ($product->type->value === "link")
+                                <div class="card-body">
+
+                                    @if ($product->thumbnail)
+                                    <img src="{{ Storage::url($product->thumbnail) }}" style="width: 64px; height: 64px;" class="card-img-top rounded-2" alt="...">
+                                        <span>bergambar</span>
+                                    @endif
+                                    <span class="fw-bold">LINK - {{ $product->layout->value }}</span>
+                                </div>
+                            @else
+                                <div class="card-body">
+                                    @if ($product->thumbnail)
+                                        <span>bergambar</span>
+                                    @endif
+                                    <span class="fw-bold">DIGITAL - {{ $product->layout->value }}</span>
+
+                                </div>
+                            @endif
+                        @if ($product->layout->value == "large")
+
+                            @if ($product->thumbnail)
+                            <img src="{{ Storage::url($product->thumbnail) }}" style="width: 100%; height: 240px;" class="card-img-top" alt="...">
+                            @endif
+                            <img src="{{ ($product->thumbnail) ? Storage::url($product->thumbnail) : asset('assets/user2.jpg') }}" style="width: 100%; height: 240px;" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h6>{{ $product->name }} | LARGE</h6>
                                 <span><strong>RP. {{ $product->min_price }}</strong></span>
                             </div>
-                        </div>
 
                         @elseif ($product->layout === "grid")
                         <span>GRID</span>
 
                         @else
-                        <div class="card {{ ($user->theme == "light") ? 'bg-white' : 'bg-dark text-white' }}">
                             <div class="card-body text-center">
-                                <span>{{ $product->name }} | DEFAULT</span>
+                                <span>{{ $product->name }} | {{ $product->layout->value }}</span>
                             </div>
-                        </div>
                         @endif
-                        
+                        </div>
                     </a>
                 </div>
-                    {{-- @dump($product->name) --}}
-                @endforeach
+                @endforeach --}}
+
+
                 {{-- <div class="col-12">
                     <a href="/awkarin/detail" class="text-decoration-none">
-                        <div class="card bg-dark-cover">
-                            <div class="card-body text-center">
-                                <span>Lorem ipsum adipisicing elit. Repellat!</span>
-                            </div>
-                        </div>
-                    </a>
-                </div> --}}
-                {{-- <div class="col-12">
-                    <a href="/awkarin/detail" class="text-decoration-none">
-                        <div class="card bg-dark-cover">
+                        <div class="card">
                             <div class="card-body d-flex flex-start align-items-center gap-3">
                                 <img src="{{ asset('assets/user1.jpg') }}" style="width: 4rem; height: 4rem;" class="card-img-top" alt="...">
                                 <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat!</span>
@@ -468,7 +329,7 @@
                     <div class="row">
                         <div class="col">
                             <a href="/awkarin/detail" class="text-decoration-none">
-                                <div class="card bg-dark-cover  h-100">
+                                <div class="card h-100">
                                     <img src="{{ asset('assets/user1.jpg') }}" style="width: 100%; height: 120px;"
                                         class="card-img-top object-fit-cover" alt="...">
                                     <div class="card-body">
@@ -480,7 +341,7 @@
                         </div>
                         <div class="col">
                             <a href="/awkarin/detail" class="text-decoration-none">
-                                <div class="card bg-dark-cover h-100">
+                                <div class="card h-100">
                                     <img src="{{ asset('assets/user3.jpg') }}" style="width: 100%; height: 120px;"
                                         class="card-img-top object-fit-cover" alt="...">
                                     <div class="card-body">
@@ -529,7 +390,21 @@
                         </div>
                     </div>
                 </div> --}}
-            </div>
+            {{-- </div> --}}
         </div>
     </section>
 @endsection
+@push('scripts')
+    <script>
+        const body = document.getElementsByTagName('body')[0]
+        const card = $('.list-products .card');
+        if ($('body').hasClass('bg-dark')) {
+            // $('')
+            $('.list-products .card').addClass(['bg-dark','text-white'])
+        }
+        // console.log($('body').hasClass('bg-light-cover'));
+        // if (body.classList.contains('bg-dark')) {
+        //     card.addClass(['bg-dark','text-white'])
+        // }
+    </script>
+@endpush
