a312979d7b6c5803794ecf5c42db0de43d23220b
kmmaniez <dzlabsid@gmail.com>
2023-11-01 08:57:30 +0700

    update user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 'username',
+        // 'name',
+        // 'email',
+        // 'phone',
+        // 'coverimage',
+        // 'photo',
+        // 'description',
+        // 'account_type',
+        // 'theme',
+        // 'password',
+    }
+}
