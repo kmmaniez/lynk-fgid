7f43cbf07b071b1f5883be867a3616a7a434537d
kmmaniez <dzlabsid@gmail.com>
2023-11-01 09:07:32 +0700

    update history settlements views

--- resources/views/creator/history.blade.php
+++ resources/views/creator/history.blade.php
@@ -5,12 +5,16 @@
         <div class="card border-0 mt-3">
             <div class="card-body p-2">
                 <h4>Settlements History</h4>
-    
-                <div class="card border-0">
-                    <div class="card-body shadow-none p-2 border-start border-success border-5">
-                        <span class="d-block fw-semibold">18 September 2023</span>
-                        <span>Rp. 250.000</span>
+
+                <div class="vstack gap-3 mt-3">
+                    @foreach ($settlements as $data)
+                    <div class="card border-0">
+                        <div class="card-body  p-2 border-start border-danger border-5">
+                            <span class="d-block fw-semibold">{{ \Carbon\Carbon::parse($data->payout_date)->translatedFormat('l') }}, {{ $data->payout_date }}</span>
+                            <span>Rp. {{ number_format($data->payout_amount,0) }}</span>
+                        </div>
                     </div>
+                    @endforeach
                 </div>
             </div>
         </div>
