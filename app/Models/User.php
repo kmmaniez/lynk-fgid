fd87f020d86c951b0624158c1133f5fd60ac5003
kmmaniez <dzlabsid@gmail.com>
2023-11-04 07:44:32 +0700

    update uuids

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,8 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasUuids;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    // HasUuids;
 
     /**
      * The attributes that are mass assignable.
