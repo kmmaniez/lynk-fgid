4bd67bd5856ba55a6ae50792e9f734cf42bfdc95
kmmaniez <dzlabsid@gmail.com>
2023-11-04 07:47:14 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
