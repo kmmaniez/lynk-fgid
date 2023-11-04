bd7fe8f2dc047823adb7ed84c0a42136af1a84a5
kmmaniez <dzlabsid@gmail.com>
2023-11-04 07:32:42 +0700

    update to delete all storage dir

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\MasterPayoutDate;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,30 +18,39 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $faker = fake('id_ID');
-        
+
+        //
+        $allDir = Storage::disk('public')->allDirectories();
+
+        if (count($allDir) > 0) {
+            for ($i=0; $i < count($allDir); $i++) { 
+                try {
+                    Storage::disk('public')->deleteDirectory($allDir[$i]);
+                    Storage::disk('public')->deleteDirectory($allDir[$i]);
+                    Storage::disk('public')->deleteDirectory($allDir[$i]);
+                } catch (\Throwable $th) {
+                    throw $th;
+                }
+            }
+        }
+        // MasterPayoutDate::create([
+        //     'initial_date' => date('Y-m-d')
+        // ]);
         $this->call([
             // PermissionSeeder::class,
             // ProductSeeder::class,
             // TransactionSeeder::class,
         ]);
-        for ($i=0; $i < 10; $i++) { 
-            $users = User::create([
-                'username'  => $faker->userName(),
-                'name'      => $faker->name(),
-                'email'     => $faker->email(),
-                // 'phone' => $faker->phoneNumber(),
-                'description' => $faker->paragraph('1'),
-                'password'  => Hash::make('password')
-            ]);
-            $users->assignRole('creator');
-        }
-        // \App\Models\User::factory()->create([
-        //      'name' => 'Admin',
-        //      'email' => 'admin@gmail.com',
-        // ]);
-
-        // $this->call([
-        //     ProductSeeder::class,
-        // ]);
+        // for ($i=0; $i < 10; $i++) { 
+        //     $users = User::create([
+        //         'username'  => $faker->userName(),
+        //         'name'      => $faker->name(),
+        //         'email'     => $faker->email(),
+        //         // 'phone' => $faker->phoneNumber(),
+        //         'description' => $faker->paragraph('1'),
+        //         'password'  => Hash::make('password')
+        //     ]);
+        //     $users->assignRole('creator');
+        // }
     }
 }
