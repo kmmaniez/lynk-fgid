cbdecd496debc75d1ab3fc4628400298b7a57557
kmmaniez <dzlabsid@gmail.com>
2023-11-05 20:39:56 +0700

    update admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
 
             <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Dashboard Admin {{ auth()->user()->name }}</h1>
+                <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
             </div>
 
             <!-- Content Row -->
@@ -37,6 +37,55 @@
                                 <div class="col mr-2">
                                     <div class="text-lg font-weight-bold text-primary text-uppercase mb-1">Total Products</div>
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $total_product }}</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-shopping-bag fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-3 mb-4">
+                    <div class="card border-bottom-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-lg font-weight-bold text-primary text-uppercase mb-1">Total Item Sold</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $total_item_paid }}</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-shopping-bag fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-3 mb-4">
+                    <div class="card border-bottom-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-lg font-weight-bold text-primary text-uppercase mb-1">Total Transaction</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $total_transaction }}</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-shopping-bag fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- Total payout -->
+                <div class="col-3 mb-4">
+                    <div class="card border-bottom-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-lg font-weight-bold text-primary text-uppercase mb-1">Total Payouts</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. {{ number_format($total_payout_amount,0,0,'.') }}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-shopping-bag fa-2x text-gray-300"></i>
