e69cd4a1ba04796b773050ebc4a0cce8ba72de09
kmmaniez <dzlabsid@gmail.com>
2023-11-07 07:45:18 +0700

    update base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function sendResponse(string $message = 'Success', int $status = 200, ...$data)
+    {
+        return response()->json([
+            'messages' => $message,
+            ...$data
+        ], $status);
+    }
 }
