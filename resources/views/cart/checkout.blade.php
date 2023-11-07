e2b0b865e8d200087afe0265bde6c06e6c634561
kmmaniez <dzlabsid@gmail.com>
2023-11-07 07:21:09 +0700

    add views cart customer

---
+++ resources/views/cart/checkout.blade.php
@@ -0,0 +1,201 @@
+@extends('layouts.products')
+
+@section('produk')
+    <section id="checkout" class="">
+
+        <div class="card border-0 h-100" style="border-radius: 0">
+            <div class="card-body">
+                <div class="card">
+                    <div class="card-body">
+                        <small>Order Summary</small>
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>Total Item</td>
+                                    <td class="text-end" id="totalitem"></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" class="p-0">
+                                        <div class="accordion" id="accordionPanelsStayOpenExample">
+                                            <div class="accordion-item border-0">
+                                                <h2 class="accordion-header">
+                                                    <button class="accordion-button bg-white shadow-none" type="button"
+                                                        data-bs-toggle="collapse"
+                                                        data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                                                        aria-controls="panelsStayOpen-collapseOne">
+                                                        List Item
+                                                    </button>
+                                                </h2>
+                                                <div id="panelsStayOpen-collapseOne"
+                                                    class="accordion-collapse collapse show">
+                                                    <div class="accordion-body py-0 px-2">
+
+                                                        <div class="hstack justify-content-between">
+                                                            <ol class="mt-2" id="cartitems">
+                                                                {{-- @foreach ($cartitems as $item)
+                                                                <li><span>{{ $item->name }} ({{ $item->quantity }}x)</span></li>
+                                                                <li><span>Valorant cheat (2x)</span></li>
+                                                                <li><span>Ninja Saga (1x)</span></li>
+                                                                @endforeach --}}
+                                                            </ol>
+    
+                                                            <ul class="mt-2" id="cartprice" style="list-style-type: none">
+                                                                {{-- @foreach ($cartitems as $item)
+                                                                <li><span>Rp. {{ $item->quantity * $item->price }}</span></li>
+                                                                <li><span>Rp. 10.000</span></li>
+                                                                <li><span>Rp. 30.000</span></li>
+                                                                @endforeach --}}
+                                                            </ul>
+
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Fee platform</td>
+                                    <td class="text-end" id="feeplatform"></td>
+                                </tr>
+                                <tr>
+                                    <th>Total</th>
+                                    <td class="text-end fw-bold" id="totalprice"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="card border-0 mt-2">
+                    <div class="card-body card-form-input">
+                        
+                        <form action="{{ route('transaction.store') }}" method="post">
+                            @csrf
+
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input class="form-control shadow-none @error('email') is-invalid @enderror" type="email" name="email" id="email" required>
+                                @error('email')
+                                <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+
+                            <div class="form-group mt-2">
+                                <label for="name">Name</label>
+                                <input class="form-control shadow-none @error('name') is-invalid @enderror" type="text" name="name" id="name" required>
+                                @error('name')
+                                <small class="text-danger">{{ $message }}</small>
+                                @enderror    
+                            </div>
+
+                            <div class="payment-method mt-3">
+                                <div class="form-group">
+                                    <label for="payment">Pilih Pembayaran</label>
+                                    <div class="hstack mt-2" id="choose_payment">
+                                        <label>
+                                            <input class="form-check-input" type="radio" id="payment" name="payment" value="OV">
+                                            <img src="{{ asset('assets/payments-icon/ovo.jpg') }}" alt="ovo">
+                                        </label>
+                                        <label>
+                                            <input class="form-check-input" type="radio" id="payment" name="payment" value="SP">
+                                            <img src="{{ asset('assets/payments-icon/qris.png') }}" alt="qris">
+                                        </label>
+                                        <label>
+                                            <input class="form-check-input" type="radio" id="payment" name="payment" value="SA">
+                                            <img src="{{ asset('assets/payments-icon/shopeepay.png') }}" alt="shopee">
+                                        </label>
+                                        <label>
+                                            <input class="form-check-input" type="radio" id="payment" name="payment" value="BC">
+                                            <img src="{{ asset('assets/cover-dark.png') }}" alt="bca">
+                                        </label>
+                                    </div>
+                                </div>
+                                @if (session()->has('payment'))
+                                <small class="text-danger">{{ session()->get('payment') }}</small>
+                                @endif
+                            </div>
+                            
+                            <div class="button-group mt-4">
+                                <button id="btnPay" class="btn text-white bg-danger bg-gradient w-100"></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+            {{-- <div class="card-footer border-0">
+                <a href="#" id="btnPay" class="btn text-white bg-danger bg-gradient w-100"></a>
+                <a href="{{ route('public.user', $userProduct[0]->username) }}" id="btnAddToCart" class="btn btn-default w-100 mt-1">Back to Shopping</a>
+            </div> --}}
+        </div>
+    </section>
+@endsection
+@push('scripts')
+    <script>
+        const btnPayment = $('#btnPay');
+        const feePlatform = $('#feeplatform');
+        const cartItems = $('#cartitems');
+        const cartPrice = $('#cartprice');
+        const totalItem = $('#totalitem');
+        const totalPrice = $('#totalprice');
+
+        const url = window.location.href;
+        let pattern = /order_id=([^&]*)/;
+        let match = url.match(pattern)
+        $(function(){
+            fetchCart()
+        })
+        $('#btnPay').on('click',(e) => {
+            e.preventDefault();
+            const link = `<input hidden name="cart" value="${match[1]}">`;
+            $('#btnPay').parent().parent().prepend(link)
+            $('#btnPay').closest('form').submit()
+        })
+
+        $('#choose_payment').children().on('change', (e) =>{
+            $.ajax({
+                url: window.location.href+'&type='+e.target.value,
+                method: 'GET',
+                cache: false,
+                success: (res) => {
+                    console.log(res);
+                    const {cart} = res;
+                    btnPayment.text('Bayar Sekarang - Rp. '+res.payment_fee)
+                    totalPrice.text(res.payment_fee)
+                    feePlatform.text(res.fees)
+                },
+                error: (res) => {
+                    console.log(res);
+                }
+            })
+        })
+
+        function fetchCart() {
+            $.ajax({
+                url: "{{  route('cart.getitems')  }}?user_id="+match[1],
+                method: 'GET',
+                success: (res) => {
+                    console.log(res);
+                    const {cart} = res.data;
+                    btnPayment.text('Bayar Sekarang - Rp. '+res.data.total_price)
+                    totalItem.text(res.data.total_item)
+                    totalPrice.text(res.data.total_price)
+                    feePlatform.text(res.data.payment_fee)
+                    
+                    for (const product in cart) {
+                        const listItems = `<li><span>${cart[product].name} (${cart[product].quantity}x)</span></li>`;
+                        const listPrice = `<li><span>Rp. ${cart[product].price}</span></li>`;
+
+                        cartPrice.append(listPrice)
+                        cartItems.append(listItems)
+                    }
+                },
+                error: (res) => {
+                    console.log(res);
+                }
+            })
+        }
+    </script>
+@endpush
---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,170 @@
+@extends('layouts.products')
+@push('styles')
+    <style>
+        .header span{
+            user-select: none;
+            color: #fff;
+            text-shadow: 0 0 10px rgb(38, 38, 38);
+        }
+        #coverprofile{
+            /* content: ''; */
+            /* background: url('{{ asset("assets/user6.jpg") }}'); */
+            /* object-fit: contain; */
+            /* background-size: 100%; */
+            /* background-position: center; */
+            /* background-repeat: no-repeat; */
+            position: absolute;
+            width: 800px;
+            height: 100px; 
+            left: -6px;
+            z-index: -1;
+            /* size cover 800 x 200 */
+        }
+        #coverprofile::after{
+            content: '';
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            z-index: 1;
+            left: 0;
+            background-color: rgba(25, 25, 25, 0.615);
+        }
+        .card-cover{
+            /* background-color: red; */
+            overflow: hidden;
+        }
+        section{
+            min-height: calc(100% - 4rem);
+        }
+        #tes{
+            /* 800 x 200 profile */
+            /* width: 100%; */
+            width: 800px;
+            height: 200px;
+            position: absolute;
+            object-fit: fill;
+            z-index: -1;
+        }
+        .card-cover::after{
+            content: '';
+            width: 100%;
+            height: 100%;
+            top: 0;
+            position: absolute;
+            left: 0;
+            background-color: rgba(25, 25, 25, 0.615);
+            z-index: -1;
+        }
+        .list-products{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            row-gap: 8px;
+        }
+        .item.default{
+            /* flex: 0 0 48%; */
+            flex: 0 0 calc(100%);
+
+            height: max-content;
+        }
+        .item.large{
+            flex-basis: 100%;
+            height: 400px;
+        }
+        .item.large .card{
+            /* height: 100%; */
+        }
+        .item.large .card .card-body > img{
+            /* width: 100%; */
+            /* height: 200px; */
+        }
+        .item.grid{
+            /* flex: 0 0 calc(100%); */
+            flex: 0 0 48%;
+            /* background-color: bisque; */
+        }
+        @media screen and (max-width: 800px){
+            .list-products{
+                width: 100%;
+            }
+            .item.grid{
+                flex: 0 0 calc(100%);
+            }
+            .item{
+                flex: 0 0 48%; 
+                /* background-color: green; */
+            }
+        }
+    </style>
+@endpush
+@section('produk')
+    <section id="wrap">
+        <div class="header">
+            <div class="card card-cover rounded-0 border-0 text-center position-relative" style="z-index: 1">
+                <img id="tes" src="{{ ($user->coverimage) ? Storage::url($user->coverimage) : asset('assets/cover-default.jpg') }}" alt="" srcset="">
+                <div class="card-body vstack gap-3 align-items-center">
+                    <img src="{{ ($user->photo) ? Storage::url($user->photo) : asset('assets/profile-default.png') }}" style="width: 5rem; height: 5rem;" 
+                        class="rounded-circle border border-secondary-subtle border-3 object-fit-cover" alt="...">
+                    <span><strong>{{ '@'.$user->username }}</strong></span>
+                    <span>{{ $user->description }}</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="list-products mt-3">
+            <!-- NEW -->
+            @foreach ($products as $product)
+            <div 
+            class="item
+            @if ($product->layout->value == 'default')
+            default
+            @elseif ($product->layout->value == 'large')
+            large
+            @else
+            grid
+            @endif
+            
+            ">
+            {{-- <div class="item {{ ($product->layout->value === 'default') ? 'grid' : '' }}"> --}}
+                <a href="{{ ($product->type->value == 'link') ? $product->url : route('products.detailuser', [$user->username,$product->slug]) }}" class="text-decoration-none">
+                    <div class="card">
+                        <div class="card-body {{ ($product->type->value == 'link') ? 'd-flex gap-2 align-items-center' : '' }}">
+                            @if ($product->type->value === 'link')
+                                @if ($product->thumbnail)
+                                <img src="{{ Storage::url($product->thumbnail) }}" style="width: 64px; height: 64px;" class="card-img-top rounded-2">
+                                @endif
+                                <h6 class="mt-3 {{ ($product->type->value == 'link') ? 'flex-grow-1 text-center' : '' }}">{{ $product->name }} {{ $product->type->value }}</h6>
+                            @else
+                                @if ($product->thumbnail)
+                                <img src="{{ Storage::url('products/digital/'.$product->thumbnail) }}" style="width: 100%; {{ ($product->layout->value == 'large') ? 'height:300px' : 'height:120px'  }};" class="rounded-1 object-fit-cover">
+                                @else
+                                <img src="{{ ($user->theme == 'light') ? asset('assets/cover-white.png') : asset('assets/cover-dark.png') }}" style="width: 100%; height: 120px;" class="rounded-1 object-fit-cover">
+                                @endif
+                                <h6 class="mt-3">{{ $product->name }}</h6>
+                                {{-- <h6 class="{{ ($product->thumbnail) ? 'mt-3' : '' }}">{{ $product->name }} {{ $product->type->value }}</h6> --}}
+                                <span><strong>Rp. {{ $product->min_price }}</strong></span>
+                            @endif
+                            {{-- <small>{{ $product->id }}</small> --}}
+                        </div>
+                    </div>
+                </a>
+            </div>
+                
+            @endforeach
+        </div>
+    </section>
+@endsection
+@push('scripts')
+    <script>
+        const body = document.getElementsByTagName('body')[0]
+        const card = $('.list-products .card');
+        if ($('body').hasClass('bg-dark')) {
+            // $('')
+            $('.list-products .card').addClass(['bg-dark','text-white'])
+        }
+        // console.log($('body').hasClass('bg-light-cover'));
+        // if (body.classList.contains('bg-dark')) {
+        //     card.addClass(['bg-dark','text-white'])
+        // }
+    </script>
+@endpush
