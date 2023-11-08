418db19d7ec1577e6f6ace858022f9e70c0c5e3b
kmmaniez <dzlabsid@gmail.com>
2023-11-08 11:36:10 +0700

    update controller dashboard by admin

--- app/Http/Controllers/Admin/DashboardAdminController.php
+++ app/Http/Controllers/Admin/DashboardAdminController.php
@@ -15,13 +15,21 @@ class DashboardAdminController extends Controller
     
     public function index()
     {
-        $total_user = User::all()->count();
-        $total_product = Product::all()->count();
-        $total_item_paid = Transaction::all()->where('payment_status','paid')->count();
-        $total_transaction = Transaction::all()->count();
-        $total_payout_amount = Settlement::all()->sum('payout_amount');
+        $total_user = User::count();
+        $user_rank = Settlement::with(['users'])->limit(5)->orderBy('payout_amount','DESC')->get();
+
+        $total_product = Product::count();
+        $total_product_digital = Product::where('type','digital')->count();
+        $total_product_link = Product::where('type','link')->count();
+
+        $total_item_paid = Transaction::where('payment_status','paid')->count();
+        $total_transaction = Transaction::count();
+        $total_transaction_amount = Transaction::sum('total_price');
+        $total_payout_amount = Settlement::sum('payout_amount');
+        
         return view('admin.dashboard', compact(
-            'total_user','total_product','total_payout_amount','total_item_paid','total_transaction'
+            'total_user','total_product','total_payout_amount','total_item_paid','total_transaction','user_rank',
+            'total_transaction_amount','total_product_digital','total_product_link'
         ));
     }
 
